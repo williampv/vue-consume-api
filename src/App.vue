1a0bc1cbbf9953b61d9f9d1e16395aa895b51b22
William <williampv@gmail.com>
2023-03-15 15:17:04 -0500

    axios setup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,40 @@
 <template>
   <div>
-    <h1>Test</h1>
+    <h1 class="title">Vue API</h1>
+    <button @click="fetch">GET</button>
+
+    <div v-for="character of characters" v-bind:key="character.id">
+      {{ character.name }}
+    </div>
   </div>
 </template>
 
 <script>
+// libraries
+import axios from "axios";
 
+export default {
+  data: function() {
+    return {
+      characters: []
+    }
+  },
+
+  methods: {
+    fetch(){
+      let result = axios
+        .get("https://rickandmortyapi.com/api/character")
+        .then(res => {
+          this.characters = res.data.results
+          console.log(res.data)
+        })
+        .catch(err => {
+          console.log(err)
+        }) 
+      
+    }
+  }
+}
 </script>
 
 <style scoped>
