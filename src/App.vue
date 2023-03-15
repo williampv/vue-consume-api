39346e45e4193b75d5fda5e7b478ec28ab62c427
William <williampv@gmail.com>
2023-03-15 15:59:53 -0500

    cards created

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <template>
   <div>
-    <h1 class="title">Vue API</h1>
-    <button @click="fetch">GET</button>
+    <div class="hero is-white is-gradient is-bold">
 
-    <div v-for="character of characters" v-bind:key="character.id">
-      {{ character.name }}
+      <div class="hero-body">
+        <h1 class="title">
+          <span class="has-text-success">R&M</span>
+          <span class="subtitle">Characters</span>
+        </h1>
+
+        <button class="button is-success is-rounded" @click="fetch">GET</button>
+      </div>
+
     </div>
+
+    <div class="container">
+      <div class="columns is-desktop is-mobile is-tablet is-multiline is-centered">
+        <div class="column is-12-mobile is-4-desktop is-4-tablet" v-for="character of characters" v-bind:key="character.id">
+          <div class="card">
+            <div class="card-header"><img v-bind:src="character.image" v-bind:alt="character.name"></div>
+            <div class="card-content">
+              <h3 class="title is-size-4">{{ character.name }}</h3>
+              <button class=" button is-success is-rounded is-small">See more</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
