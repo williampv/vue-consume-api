db1db4b0695e3665700ac3c0ca510580d16fef7c
William <williampv@gmail.com>
2023-03-15 17:00:34 -0500

    pagination added

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,24 @@
       <div 
         class="columns is-desktop is-mobile is-tablet is-multiline is-centered"
       >
-        <character v-for="character of characters" v-bind:key="character.id" v-bind:character = "character"></character>
+        <character 
+          v-for="character of characters" 
+          v-bind:key="character.id" 
+          v-bind:character = "character">
+        </character>
       </div>
+
+      <nav class="pagination is-centered" role="navegation" aria-label="pagination">
+        <a class="pagination-previous" v-on:click="changePage( page - 1 )">Previous</a>
+
+        <ul class="pagination-list">
+          <li>
+            <a class="pagination-link is-current">{{ page }}</a>
+          </li>
+        </ul>
+
+        <a class="pagination-next" v-on:click="changePage( page + 1 )">Next</a>
+      </nav>
     </div>
 
   </div>
@@ -35,22 +51,35 @@ export default {
   },
   data: function() {
     return {
-      characters: []
+      characters: [],
+      page: 1,
+      pages: 1
     }
   },
-
+  created() {
+    this.fetch()
+  },
   methods: {
     fetch(){
+      const params = {
+        page: this.page
+      }      
+
       let result = axios
-        .get("https://rickandmortyapi.com/api/character")
+        .get("https://rickandmortyapi.com/api/character/", { params })
         .then(res => {
           this.characters = res.data.results
+          this.pages = res.data.info.pages
           console.log(res.data)
         })
         .catch(err => {
           console.log(err)
         }) 
       
+    },
+    changePage(page) {
+      this.page = page <= 0 || page > this.pages ? this.page : page
+      this.fetch()
     }
   }
 }
